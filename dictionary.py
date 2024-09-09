8f2bae83b64da0d74fc2b3f96ed15a412baa0f23
BabyX047 <hyperlaser93.lg@gmail.com>
2024-09-09 22:48:11 +0300

    103

--- dictionary.py
+++ dictionary.py
@@ -1,26 +1,3 @@
-#Basic Solution
-
-# # Create an empty dictionary to store the person's information
-# person_info = {}
-
-# user_input = input("Enter your name, age, and favorite color (separated by commas): ")
-
-# # Split the input string into a list using comma as the delimiter
-# name, age, favorite_color = user_input.split(',')
-
-# # Store the information in the dictionary
-# person_info['Name'] = name.strip()
-# person_info['Age'] = age.strip()
-# person_info['Favorite Color'] = favorite_color.strip()
-
-# # Print the dictionary
-# print(person_info)
-
-
-
-
-# Iteration one
-
 person_info = {}
 
 try:
