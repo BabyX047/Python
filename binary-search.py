4b0b831d564783df1d836956e3e6e9c43d165616
BabyX047 <hyperlaser93.lg@gmail.com>
2024-09-11 20:42:23 +0300

    102

--- binary-search.py
+++ binary-search.py
@@ -37,3 +37,23 @@ def binary_search_recursive(arr, low, high, target, steps=0):
     # Target is larger, search in the right half
     else:
         return binary_search_recursive(arr, mid + 1, high, target, steps)
+
+
+
+
+
+def get_input():
+    # Input a list of numbers from the user
+    user_list = input("Enter a list of numbers separated by spaces: \n").split()
+    arr = [int(num) for num in user_list]
+    
+    # Input the target number to search
+    target = int(input("Enter the number you want to search for: \n"))
+    
+    return arr, target
+
+def sort_array(arr):
+    # Sorting the array before binary search
+    arr.sort()
+    print(f"Sorted array: {arr}")
+    return arr
